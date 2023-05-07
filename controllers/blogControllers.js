92910366759c3293314b4bc247d83f4f0b115f4d
nirgluzman <nir.gluzman@gmail.com>
2023-05-08 00:11:52 +0200

    add PATCH by id for rating property

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -34,10 +34,29 @@ export const getBlogsByAuthor = async (req, res) => {
     const blogs = await Blog.find({ author });
 
     if (!blogs.length) {
-      return res.status(404).json({ message: "No entries found" });
+      return res.status(404).json({ error: "No entries found" });
     }
 
     return res.status(200).json({ message: blogs });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const updateRatingById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { rating } = req.body;
+    const blog = await Blog.findByIdAndUpdate(
+      id,
+      { rating },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    return res.status(200).json({ message: blog });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,11 +4,13 @@ import {
   getAllBlogs,
   createBlog,
   getBlogsByAuthor,
+  updateRatingById,
 } from "../controllers/blogControllers.js";
 
 const app = Router();
 
 app.route("/").get(getAllBlogs).post(createBlog);
-app.route("/:author").get(getBlogsByAuthor);
+app.get("/:author", getBlogsByAuthor);
+app.patch("/:id", updateRatingById);
 
 export default app;
